a58d65a12d5ee3b59347e8b43498216c9cb17be2
donny <dongook.son@yonsei.ac.kr>
2021-12-24 00:01:23 +0900

    + tabular solution for 322 leetcode

--- LeetCode/322_coin_change/dos.py
+++ LeetCode/322_coin_change/dos.py
@@ -1,6 +1,24 @@
 class Solution:
     def coinChange(self, coins, amount: int) -> int:
+        """
+        via tabulation
+        """
+        tbl = [None] * (amount+1)
+        tbl[0] = []
+        for i, v in enumerate(tbl):
+            if v is not None:
+                for c in coins:
+                    if (i+c) < (amount+1):
+                        combo = [*tbl[i], c]
+                        if tbl[i+c] is None or len(tbl[i+c]) > len(combo):
+                            tbl[i+c] = combo
+        result = tbl[amount]
+        return len(result) if result is not None else -1
+
         def get_shortest_combo(coins, amount, mem=None):
+            """
+            via memoization
+            """
             if mem is None:
                 mem = {}
             if amount in mem:
@@ -8,20 +26,19 @@ class Solution:
             if amount == 0:
                 return []
             if amount < 0:
-                return -1
-            shortest_combo = -1
+                return None
+            shortest_combo = None
             for c in coins:
                 remainder = amount - c
                 remainder_combo = get_shortest_combo(coins, remainder, mem)
-                if remainder_combo != -1:
+                if remainder_combo is not None:
                     combo = [*remainder_combo, c]
-                    if shortest_combo == -1 or len(combo) < len(shortest_combo):
+                    if shortest_combo is None or len(combo) < len(shortest_combo):
                         shortest_combo = combo
             mem[amount] = shortest_combo
             return mem[amount]
         result = get_shortest_combo(coins, amount)
-        return len(result) if result != -1 else -1
-
+        return len(result) if result is not None else -1
 
 
 if __name__ == '__main__':
