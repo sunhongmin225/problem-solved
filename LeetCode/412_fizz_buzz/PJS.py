6dcc94d4455ee64f71fef37701aeffa38776379f
soleee99 <notredamejin@naver.com>
2022-01-08 20:34:02 +0900

    Add Solutions PJS for LeetCode_412

---
+++ LeetCode/412_fizz_buzz/PJS.py
@@ -0,0 +1,15 @@
+class Solution:
+    def fizzBuzz(self, n: int) -> List[str]:
+        fizzbuzz = []
+        for num in range(n):
+            if (num+1) % 15 == 0:
+                fizzbuzz.append("FizzBuzz")
+            elif (num+1) % 5 == 0:
+                fizzbuzz.append("Buzz")
+            elif (num+1) % 3 == 0:
+                fizzbuzz.append("Fizz")
+            else:
+                fizzbuzz.append(str(num+1))
+                
+        return fizzbuzz
+        
