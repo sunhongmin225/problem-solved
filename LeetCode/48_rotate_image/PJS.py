aa644081e4919ec294c8a9bc521c8e2c31556393
soleee99 <notredamejin@naver.com>
2022-01-08 20:57:39 +0900

    Add Solutions PJS for LeetCode_48

---
+++ LeetCode/48_rotate_image/PJS.py
@@ -0,0 +1,16 @@
+class Solution:
+    def rotate(self, matrix: List[List[int]]) -> None:
+        """
+        Do not return anything, modify matrix in-place instead.
+        """       
+        n = len(matrix)
+        for i in range(n):
+            matrix[i].reverse()
+        
+        # now we can take the symmetric version of 'matrix'
+        for i in range(n):
+            for j in range(n-i):
+                tmp = matrix[i][j]
+                matrix[i][j] = matrix[n-1-j][n-1-i]
+                matrix[n-1-j][n-1-i] = tmp
+       
