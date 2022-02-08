0b87bd977137bb2333601d51392b830dac33a047
ttoru96 <ttoru96jk@gmail.com>
2022-02-09 00:59:00 +0900

    Fix LeetCode 378

--- LeetCode/378_kth_smallest_element_in_a_sorted_matrix/JSY.py
+++ LeetCode/378_kth_smallest_element_in_a_sorted_matrix/JSY.py
@@ -1,24 +1,48 @@
-from queue import PriorityQueue
 from typing import List
 
 class Solution:
     def kthSmallest(self, matrix: List[List[int]], k: int) -> int:
-        que = PriorityQueue()
+        N = len(matrix)
+        if N == 1: return matrix[0][0]
+        elif k == 1: return matrix[0][0]
+        else:
+            nx = [0, 1]
+            ny = [1, 0]
 
-        ROWS, COLS = len(matrix), len(matrix[0])
-        for i in range(ROWS):
-            for j in range(COLS):
-                que.put(matrix[i][j])
-        
-        ans = 0
-        for i in range(k):
-            que.get()
-            if i == k - 1:
-                ans = que.get()
+            candidates = []
+            x, y, smallest = 0, 0, 0
 
-        return ans
+            while (k > 1):
+                for i in range(2):
+                    r = x + nx[i]
+                    c = y + ny[i]
+                
+                    if r < N and c < N and (r, c) not in candidates:
+                        candidates.append((r, c))
+
+                # Find the smallest element among candidates
+                random_item = candidates[0]
+                smallest = matrix[random_item[0]][random_item[1]]
+                x = random_item[0]
+                y = random_item[1]
+
+                for candidate in candidates:
+                    x_ = candidate[0]
+                    y_ = candidate[1]
+                    if matrix[x_][y_] < smallest:
+                        smallest = matrix[x_][y_]
+                        x = x_
+                        y = y_
+
+                candidates.remove((x, y))
+                k -= 1
+            
+            return smallest
 
 if __name__ == "__main__":
+    # matrix = [[-5]]
+    # k = 1
+
     matrix = [[1,5,9],[10,11,13],[12,13,15]]
     k = 8
 
